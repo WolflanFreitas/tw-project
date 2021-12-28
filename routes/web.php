c1d18db02ef7d347061c548d9298043930963ad6
Wolflan Freitas <wolflanf@gmail.com>
2021-12-28 10:40:59 -0300

    Básico sobre rotas no Laravel

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,42 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/**
+    Route::get('/', function () {
+        return view('welcome');
+    });
+*/
+
 Route::get('/', function () {
-    return view('welcome');
+    return "Conteúdo dinâmico HOME";
+});
+
+Route::get('/sobre', function () {
+    return "Conteúdo dinâmico SOBRE";
+});
+
+Route::get('/contato', function () {
+    return "Conteúdo dinâmico CONTATO";
+});
+
+Route::get('/servicos', function () {
+    return "Conteúdo dinâmico SERVIÇO";
+});
+
+Route::get('/servico/{id}', function (int $id) {
+    $servicos = [
+        1 => [
+            'nome' => 'Lavagem de Roupa',
+            'descricao' => 'descricao muito longa...'
+        ],
+        2 => [
+            'nome' => 'Lavagem de Coberta',
+            'descricao' => 'descricao muito longa...'
+        ],
+        3 => [
+            'nome' => 'Lavagem de Palitó',
+            'descricao' => 'descricao muito longa...'
+        ]
+    ];
+    return $servicos[$id]['nome'];
 });
